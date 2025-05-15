33964f4f943faf1682481d61f74baae678834142
Javor <107422577+javornovosel@users.noreply.github.com>
2025-05-15 15:15:53 +0200

    Revert "SBFX-52 Create package for MP-Bfx-Reports"

--- src/Xiphias/Zed/BfxReportsMerchantPortalGui/Presentation/Components/public-api.ts
+++
@@ -1,9 +0,0 @@
-export * from './app/components.module';
-export * from './app/bfx-reports/bfx-reports.component';
-export * from './app/bfx-reports/bfx-reports.module';
-export * from './app/bfx-reports-table/bfx-reports-table.component';
-export * from './app/bfx-reports-table/bfx-reports-table.module';
-export * from './app/bfx-reports-iframe/bfx-reports-iframe.component';
-export * from './app/bfx-reports-iframe/bfx-reports-iframe.module';
-
-
